ebb552e5b7d29622b4b3ba49c0842f6c62972ed8
Emmanuel uma <umaemmanuel62@gmail.com>
2023-01-26 17:16:25 +0100

    done

---
+++ brain.js
@@ -0,0 +1,89 @@
+const searchBtn = document.getElementById('search-btn');
+const searchBody = document.getElementById('searchBody');
+const innerClose = document.getElementById('innerClose');
+const searchInput = document.getElementById('searchInput');
+const bgX = document.querySelectorAll('.bgX');
+const menuBtn = document.getElementById('menu-btn');
+const navClose = document.getElementById('nav-close');
+const nav = document.getElementById('nav');
+
+searchBtn.addEventListener('click', e => {
+    searchBody.classList.toggle('active');
+    if(searchInput.style.height !== '0px') {
+        searchInput.style.height = '40px'
+    }else{
+        searchInput.style.height = '0px';
+    }
+});
+innerClose.addEventListener('click', e =>{
+    searchBody.classList.toggle('active');
+    searchInput.classList.toggle('active');
+});
+
+function made(main) {
+    main.addEventListener('click', e => {
+        bgX.forEach(ele => {
+            ele.classList.toggle('active');
+        })
+    });
+}
+made(menuBtn);
+made(navClose);
+
+const link = document.querySelectorAll('.link');
+link.forEach(icon => {
+    icon.addEventListener('click', e => {
+        bgX.forEach(ele => {
+            ele.classList.toggle('active');
+        })
+    });
+}) 
+
+const home = document.getElementById('home')
+const fa = document.querySelectorAll('.side0');
+let firstBg = document.getElementById('firstBg');
+let mainHeight;
+mainHeight = firstBg.clientWidth;
+fa.forEach(icons => {
+    icons.addEventListener('click', e => {
+        if(icons.id == 'next') {
+            home.scrollLeft += mainHeight;
+        }else{
+            home.scrollLeft -= mainHeight;
+        }
+    });
+});
+const skip = async() => {
+    setTimeout(() => {
+        let num1;
+        num1 = home.offsetWidth + mainHeight;
+        if (home.scrollLeft == num1){
+            home.scrollLeft = 0;
+            
+        }
+        else{
+            home.scrollLeft += mainHeight;
+        }
+        skip();
+    }, 5000);
+}
+const body = document.getElementById('body')
+window.addEventListener('load', e => {
+    skip();
+})
+
+/* const overFlow = document.getElementById('overFlow')
+const side = document.querySelectorAll('.side');
+let boxItemOne = document.getElementById('boxItemOne');
+let mainHeight1;
+mainHeight1 = boxItemOne.clientWidth + 10;
+side.forEach(icons => {
+    icons.addEventListener('click', e => {
+        if(icons.id == 'next1') {
+            overFlow.scrollLeft += mainHeight1;
+        }else{
+            overFlow.scrollLeft -= mainHeight1;
+        }
+    });
+});
+ */
---
+++ portal/brain.js
@@ -0,0 +1,49 @@
+const start = document.getElementById('start');
+const one = document.getElementById('one')
+
+    start.addEventListener('click', e => {
+        one.classList.toggle('active')
+    })
+    
+const btn = document.getElementById('btn');
+const amount = document.getElementById('amount');
+const phoneNumber = document.getElementById('phoneNumber');
+btn.addEventListener('click', e =>{
+        // Set your app credentials
+        const credentials = {
+            apiKey: '51a01d34c0c7121f237cb06a896daed8ad2585b4be794d8a62cac6ce75735bea',
+            username: 'nickiscott',
+        }
+
+        // Initialize the SDK
+        const AfricasTalking = require('africastalking')(credentials);
+
+        // Get the airtime service
+        const airtime = AfricasTalking.AIRTIME;
+
+        function sendAirtime() {
+            const options = {
+                recipients: [{
+                    phoneNumber: phoneNumber.value,
+                    currencyCode: "NGN",
+                    amount: amount.value
+                }]
+            };
+
+            // That’s it hit send and we’ll take care of the rest
+            airtime.send(options)
+                .then(response => {
+                    console.log(response);
+                }).catch(error => {
+                    console.log(error);
+                });
+        }
+
+        sendAirtime();
+        console.log(1)
+})
+
+
+
+
+
